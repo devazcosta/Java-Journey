06898436957e39ca29ea8049074f3bd59b700a25
Débora Vaz Costa <94804364+devazcosta@users.noreply.github.com>
2026-01-20 09:50:47 -0300

    Add files via upload

---
+++ EntradaSaidaPadraoExemplos.java
@@ -0,0 +1,17 @@
+public class EntradaSaidaPadraoExemplos {
+    public static void main ( String[] args ) {
+        /* Neste prgrama iremos fazer algumas declarações de variáveis.
+         */
+        int idade;                  // variável inteira de nome 'idade'
+        char letra;                 // variável caractere de nome 'letra'
+        double altura;              // variável para decimais chamada 'altura'
+
+        int paresDeBrincos = 14;    // Aqui eu já declaro um valor a ser atribuído à variável
+        char letraInicial = 'D';    // letraInicial é inicializada com o caractere 'D'
+        double peso = 59.3;         // lembre de usar ponto (.) e não vírgula para indicar 
+                                    // a casa decimal de um fracionário.
+
+
+    }
+
+}
