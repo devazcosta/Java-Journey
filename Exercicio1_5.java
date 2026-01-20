1c5a976d753d675d42313cbead1b53da95f81460
Débora Vaz Costa <94804364+devazcosta@users.noreply.github.com>
2026-01-20 09:47:35 -0300

    Add files via upload

---
+++ Exercicio1_5.java
@@ -0,0 +1,18 @@
+public class Exercicio1_5 {
+    
+    public static void main( String[] args ){
+
+        System.out.println( "*********         ***           *            *" );
+        System.out.println( "*       *       *     *        ***          * *" );
+        System.out.println( "*       *      *       *      *****        *   *" );
+        System.out.println( "*       *      *       *        *         *     *" );
+        System.out.println( "*       *      *       *        *        *       *" );
+        System.out.println( "*       *      *       *        *         *     *" );
+        System.out.println( "*       *      *       *        *          *   *");
+        System.out.println( "*       *       *     *         *           * *");
+        System.out.println( "*********         * *           *            *");
+
+
+    }
+
+}
